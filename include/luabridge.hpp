d83dfe71b1437586b9acbd3c895a6f8d3e53c7f6
Vinnie Falco <vinnie.falco@gmail.com>
2012-05-07 11:52:58 -0700

    Clean up white space

--- include/luabridge.hpp
+++ include/luabridge.hpp
@@ -23,152 +23,153 @@
 */
 //==============================================================================
 
-#ifndef LUABRIDGE_HPP
-#define LUABRIDGE_HPP
+#ifndef LUABRIDGE_LUABRIDGE_HEADER
+#define LUABRIDGE_LUABRIDGE_HEADER
 
 #include <cassert>
 #include <string>
 
 #ifndef USE_OTHER_SHARED_PTR
-#	include "shared_ptr.hpp"
+#include "shared_ptr.hpp"
 #endif
 
 namespace luabridge
 {
-  template <typename T> class class__;
 
-  // scope performs registration tasks in a given Lua state
-  class scope
-  {
-  protected:
-    lua_State *L;
-    std::string name;
-  public:
-    scope (lua_State *L_, const char *name_ = "");
+template <typename T> class class__;
 
-    // Function registration
+// scope performs registration tasks in a given Lua state
+class scope
+{
+protected:
+  lua_State *L;
+  std::string name;
+public:
+  scope (lua_State *L_, const char *name_ = "");
 
-    template <typename FnPtr>
-    scope& function (const char *name, FnPtr fp);
+  // Function registration
 
-    // Variable registration.  Variables can be read/write (rw)
-    // or read-only (ro).  Varieties that access pointers directly
-    // and varieties that access through function calls are provided.
+  template <typename FnPtr>
+  scope& function (const char *name, FnPtr fp);
 
-    template <typename T>
-    scope& variable_ro (const char *name, const T *data);
-    template <typename T>
-    scope& variable_ro (const char *name, T (*get) ());
-    template <typename T>
-    scope& variable_rw (const char *name, T *data);
-    template <typename T>
-    scope& variable_rw (const char *name, T (*get) (), void (*set) (T));
+  // Variable registration.  Variables can be read/write (rw)
+  // or read-only (ro).  Varieties that access pointers directly
+  // and varieties that access through function calls are provided.
 
-    // Class registration
-
-    // For registering a class that hasn't been registered before
-    template <typename T>
-    class__<T> class_ (const char *name);
-    // For registering subclasses (the base class must also be registered)
-    template <typename T, typename Base>
-    class__<T> subclass (const char *name);
-    // For registering additional methods of a previously registered class
-    // (or subclass)
-    template <typename T>
-    class__<T> class_ ();
-
-    // !!!UNDONE: support namespaces
-  };
-
-  // class__ performs registration for members of a class
   template <typename T>
-  class class__ : public scope
-  {
-  public:
-    class__ (lua_State *L_);
-    class__ (lua_State *L_, const char *name_);
-    class__ (lua_State *L_, const char *name_, const char *basename);
-
-    // Constructor registration.  The template parameter should be passed
-    // a function pointer type; only the argument list will be used (since
-    // you can't take the address of a ctor).
-    template <typename FnPtr>
-    class__<T>& constructor ();
-
-    // Method registration
-    template <typename FnPtr>
-    class__<T>& method (const char *name, FnPtr fp);
-
-    // Property registration.  Properties can be read/write (rw)
-    // or read-only (ro).  Varieties that access member pointers directly
-    // and varieties that access through function calls are provided.
-    template <typename U>
-    class__<T>& property_ro (const char *name, const U T::* mp);
-    template <typename U>
-    class__<T>& property_ro (const char *name, U (T::*get) () const);
-    template <typename U>
-    class__<T>& property_rw (const char *name, U T::* mp);
-    template <typename U>
-    class__<T>& property_rw (const char *name,
-      U (T::*get) () const, void (T::*set) (U));
-
-    // Static method registration
-    template <typename FnPtr>
-    class__<T>& static_method (const char *name, FnPtr fp)
-    { return *(class__<T>*)&(function(name, fp)); }
-
-    // Static property registration
-    template <typename U>
-    class__<T>& static_property_ro (const char *name, const U *data)
-    { return *(class__<T>*)&(variable_ro<U>(name, data)); }
-    template <typename U>
-    class__<T>& static_property_ro (const char *name, U (*get) ())
-    { return *(class__<T>*)&(variable_ro<U>(name, get)); }
-    template <typename U>
-    class__<T>& static_property_rw (const char *name, U *data)
-    { return *(class__<T>*)&(variable_rw<U>(name, data)); }
-    template <typename U>
-    class__<T>& static_property_rw (const char *name, U (*get) (),
-      void (*set) (U))
-    { return *(class__<T>*)&(variable_rw<U>(name, get, set)); }
-
-    // !!!UNDONE: allow inheriting Lua classes from C++ classes
-  };
-
-  // Convenience functions: like lua_getfield and lua_setfield, but raw
-  inline void rawgetfield (lua_State *L, int idx, const char *key)
-  {
-    lua_pushstring(L, key);
-    if (idx < 0) --idx;
-    lua_rawget(L, idx);
-  }
-  inline void rawsetfield (lua_State *L, int idx, const char *key)
-  {
-    lua_pushstring(L, key);
-    lua_insert(L, -2);
-    if (idx < 0) --idx;
-    lua_rawset(L, idx);
-  }
-
-  // Prototypes for implementation functions implemented in luabridge.cpp
-  void *checkclass (lua_State *L, int idx, const char *tname,
-    bool exact = false);
-  int indexer (lua_State *L);
-  int newindexer (lua_State *L);
-  int m_newindexer (lua_State *L);
-  void create_static_table (lua_State *L);
-  void lookup_static_table (lua_State *L, const char *name);
-
-  // Predeclare classname struct since several implementation files use it
+  scope& variable_ro (const char *name, const T *data);
   template <typename T>
-  struct classname;
-  extern const char *classname_unknown;
+  scope& variable_ro (const char *name, T (*get) ());
+  template <typename T>
+  scope& variable_rw (const char *name, T *data);
+  template <typename T>
+  scope& variable_rw (const char *name, T (*get) (), void (*set) (T));
+
+  // Class registration
+
+  // For registering a class that hasn't been registered before
+  template <typename T>
+  class__<T> class_ (const char *name);
+  // For registering subclasses (the base class must also be registered)
+  template <typename T, typename Base>
+  class__<T> subclass (const char *name);
+  // For registering additional methods of a previously registered class
+  // (or subclass)
+  template <typename T>
+  class__<T> class_ ();
+
+  // !!!UNDONE: support namespaces
+};
+
+// class__ performs registration for members of a class
+template <typename T>
+class class__ : public scope
+{
+public:
+  class__ (lua_State *L_);
+  class__ (lua_State *L_, const char *name_);
+  class__ (lua_State *L_, const char *name_, const char *basename);
+
+  // Constructor registration.  The template parameter should be passed
+  // a function pointer type; only the argument list will be used (since
+  // you can't take the address of a ctor).
+  template <typename FnPtr>
+  class__<T>& constructor ();
+
+  // Method registration
+  template <typename FnPtr>
+  class__<T>& method (const char *name, FnPtr fp);
+
+  // Property registration.  Properties can be read/write (rw)
+  // or read-only (ro).  Varieties that access member pointers directly
+  // and varieties that access through function calls are provided.
+  template <typename U>
+  class__<T>& property_ro (const char *name, const U T::* mp);
+  template <typename U>
+  class__<T>& property_ro (const char *name, U (T::*get) () const);
+  template <typename U>
+  class__<T>& property_rw (const char *name, U T::* mp);
+  template <typename U>
+  class__<T>& property_rw (const char *name,
+    U (T::*get) () const, void (T::*set) (U));
+
+  // Static method registration
+  template <typename FnPtr>
+  class__<T>& static_method (const char *name, FnPtr fp)
+  { return *(class__<T>*)&(function(name, fp)); }
+
+  // Static property registration
+  template <typename U>
+  class__<T>& static_property_ro (const char *name, const U *data)
+  { return *(class__<T>*)&(variable_ro<U>(name, data)); }
+  template <typename U>
+  class__<T>& static_property_ro (const char *name, U (*get) ())
+  { return *(class__<T>*)&(variable_ro<U>(name, get)); }
+  template <typename U>
+  class__<T>& static_property_rw (const char *name, U *data)
+  { return *(class__<T>*)&(variable_rw<U>(name, data)); }
+  template <typename U>
+  class__<T>& static_property_rw (const char *name, U (*get) (),
+    void (*set) (U))
+  { return *(class__<T>*)&(variable_rw<U>(name, get, set)); }
+
+  // !!!UNDONE: allow inheriting Lua classes from C++ classes
+};
+
+// Convenience functions: like lua_getfield and lua_setfield, but raw
+inline void rawgetfield (lua_State *L, int idx, const char *key)
+{
+  lua_pushstring(L, key);
+  if (idx < 0) --idx;
+  lua_rawget(L, idx);
+}
+inline void rawsetfield (lua_State *L, int idx, const char *key)
+{
+  lua_pushstring(L, key);
+  lua_insert(L, -2);
+  if (idx < 0) --idx;
+  lua_rawset(L, idx);
+}
+
+// Prototypes for implementation functions implemented in luabridge.cpp
+void *checkclass (lua_State *L, int idx, const char *tname,
+  bool exact = false);
+int indexer (lua_State *L);
+int newindexer (lua_State *L);
+int m_newindexer (lua_State *L);
+void create_static_table (lua_State *L);
+void lookup_static_table (lua_State *L, const char *name);
+
+// Predeclare classname struct since several implementation files use it
+template <typename T>
+struct classname;
+extern const char *classname_unknown;
 
   // Include implementation files
-#	include "impl/typelist.hpp"
-#	include "impl/stack.hpp"
-#	include "impl/scope.hpp"
-#	include "impl/class.hpp"
+#include "impl/typelist.hpp"
+#include "impl/stack.hpp"
+#include "impl/scope.hpp"
+#include "impl/class.hpp"
 }
 
 
