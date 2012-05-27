af7288ebc1a7488b2b44c04be0420af433d4eb23
Vinnie Falco <vinnie.falco@gmail.com>
2012-05-27 09:48:14 -0700

    Update copyright

--- RefCountedPtr.h
+++ RefCountedPtr.h
@@ -25,22 +25,6 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
-
-  This file incorporates work covered by the following copyright and
-  permission notice:  
-
-    The Loki Library
-    Copyright (c) 2001 by Andrei Alexandrescu
-    This code accompanies the book:
-    Alexandrescu, Andrei. "Modern C++ Design: Generic Programming and Design 
-        Patterns Applied". Copyright (c) 2001. Addison-Wesley.
-    Permission to use, copy, modify, distribute and sell this software for any 
-        purpose is hereby granted without fee, provided that the above copyright 
-        notice appear in all copies and that both that copyright notice and this 
-        permission notice appear in supporting documentation.
-    The author or Addison-Welsey Longman make no representations about the 
-        suitability of this software for any purpose. It is provided "as is" 
-        without express or implied warranty.
 */
 //==============================================================================
 
