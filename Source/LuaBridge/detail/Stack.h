f50ef51f248ee0838014d64897b0fba83979914b
Justin Li <jli.justinli@gmail.com>
2013-11-15 20:45:58 -0500

    Use the correct method for pushing a string with a fixed length

--- Source/LuaBridge/detail/Stack.h
+++ Source/LuaBridge/detail/Stack.h
@@ -457,7 +457,7 @@ struct Stack <std::string const&>
 {
   static inline void push (lua_State* L, std::string const& str)
   {
-    lua_pushstring (L, str.c_str(), str.size());
+    lua_pushlstring (L, str.c_str(), str.size());
   }
 
   static inline std::string get (lua_State* L, int index)
