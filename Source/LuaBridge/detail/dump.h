6a35eaee12b7d847b1cfe8db6116f573b5e9af9f
Dmitry Ledentsov <dmlled@yahoo.com>
2014-05-28 21:46:18 +0200

    inline dump

--- Source/LuaBridge/detail/dump.h
+++ Source/LuaBridge/detail/dump.h
@@ -1,7 +1,7 @@
 #include <sstream>
 #include <string>
 
-std::string dumpLuaState(lua_State *L) {
+inline std::string dumpLuaState(lua_State *L) {
 	std::stringstream ostr;
 	int i;
 	int top = lua_gettop(L);
