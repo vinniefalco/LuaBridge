a73edd0b7d0bc6d5d80bf19d842155c6aec79163
Dmitry T <dmitry.a.tarakanov@gmail.com>
2018-09-11 09:43:48 +0300

    Extend LuaRef tests

--- Tests/Source/LuaRefTests.cpp
+++ Tests/Source/LuaRefTests.cpp
@@ -33,7 +33,53 @@ struct LuaRefTests : TestBase
 {
 };
 
-TEST_F (LuaRefTests, Dictionary)
+TEST_F (LuaRefTests, ValueAccess)
+{
+  runLua ("result = true");
+  ASSERT_TRUE (result ().isBool ());
+  ASSERT_TRUE (result ().cast <bool> ());
+
+  runLua ("result = 7");
+  ASSERT_TRUE (result ().isNumber ());
+  ASSERT_EQ (7u, result ().cast <unsigned char> ());
+  ASSERT_EQ (7, result ().cast <short> ());
+  ASSERT_EQ (7u, result ().cast <unsigned short> ());
+  ASSERT_EQ (7, result ().cast <int> ());
+  ASSERT_EQ (7u, result ().cast <unsigned int> ());
+  ASSERT_EQ (7, result ().cast <long> ());
+  ASSERT_EQ (7u, result ().cast <unsigned long> ());
+  ASSERT_EQ (7, result ().cast <long long> ());
+  ASSERT_EQ (7u, result ().cast <unsigned long long> ());
+
+  runLua ("result = 3.14");
+  ASSERT_TRUE (result ().isNumber ());
+  ASSERT_FLOAT_EQ (3.14f, result ().cast <float> ());
+  ASSERT_DOUBLE_EQ (3.14, result ().cast <double> ());
+
+  runLua ("result = 'D'");
+  ASSERT_TRUE (result ().isString ());
+  ASSERT_EQ ('D', result ().cast <char> ());
+  ASSERT_EQ ("D", result ().cast <std::string> ());
+  ASSERT_STREQ ("D", result ().cast <const char*> ());
+
+  runLua ("result = 'abc'");
+  ASSERT_TRUE (result ().isString ());
+  ASSERT_EQ ("abc", result ().cast <std::string> ());
+  ASSERT_STREQ ("abc", result ().cast <char const*> ());
+
+  runLua ("result = function (i) "
+          "  result = i + 1 "
+          "  return i "
+          "end");
+  ASSERT_TRUE (result ().isFunction ());
+  auto fnResult = result () (41); // Replaces result variable
+  ASSERT_TRUE (fnResult.isNumber ());
+  ASSERT_EQ (41, fnResult.cast <int> ());
+  ASSERT_TRUE (result ().isNumber ());
+  ASSERT_EQ (42, result ().cast <int> ());
+}
+
+TEST_F (LuaRefTests, DictionaryAccess)
 {
   runLua (
     "result = {"
@@ -42,13 +88,16 @@ TEST_F (LuaRefTests, Dictionary)
     "  c = 3.14,"
     "  [true] = 'D',"
     "  [8] = 'abc',"
-    "  fn = function (i) result = i end"
+    "  fn = function (i) "
+    "    result = i + 1 "
+    "    return i "
+    "  end"
     "}");
 
-  ASSERT_EQ (true, result () ["bool"].isBool ());
-  ASSERT_EQ (true, result () ["bool"].cast <bool> ());
+  ASSERT_TRUE (result () ["bool"].isBool ());
+  ASSERT_TRUE (result () ["bool"].cast <bool> ());
 
-  ASSERT_EQ (true, result () ["int"].isNumber ());
+  ASSERT_TRUE (result () ["int"].isNumber ());
   ASSERT_EQ (5u, result () ["int"].cast <unsigned char> ());
   ASSERT_EQ (5, result () ["int"].cast <short> ());
   ASSERT_EQ (5u, result () ["int"].cast <unsigned short> ());
@@ -59,22 +108,23 @@ TEST_F (LuaRefTests, Dictionary)
   ASSERT_EQ (5, result () ["int"].cast <long long> ());
   ASSERT_EQ (5u, result () ["int"].cast <unsigned long long> ());
 
-  ASSERT_EQ (true, result () ['c'].isNumber ());
+  ASSERT_TRUE (result () ['c'].isNumber ());
   ASSERT_FLOAT_EQ (3.14f, result () ['c'].cast <float> ());
   ASSERT_DOUBLE_EQ (3.14, result () ['c'].cast <double> ());
 
-  ASSERT_EQ (true, result () [true].isString ());
+  ASSERT_TRUE (result () [true].isString ());
   ASSERT_EQ ('D', result () [true].cast <char> ());
   ASSERT_EQ ("D", result () [true].cast <std::string> ());
   ASSERT_STREQ ("D", result () [true].cast <const char*> ());
 
-  ASSERT_EQ (true, result () [8].isString ());
+  ASSERT_TRUE (result () [8].isString ());
   ASSERT_EQ ("abc", result () [8].cast <std::string> ());
   ASSERT_STREQ ("abc", result () [8].cast <char const*> ());
 
-  ASSERT_EQ (true, result () ["fn"].isFunction ());
-  auto fnResult = result () ["fn"] (42); // Replaces result variable
-  ASSERT_TRUE (fnResult.isNil ());
+  ASSERT_TRUE (result () ["fn"].isFunction ());
+  auto fnResult = result () ["fn"] (41); // Replaces result variable
+  ASSERT_TRUE (fnResult.isNumber ());
+  ASSERT_EQ (41, fnResult.cast <int> ());
   ASSERT_TRUE (result ().isNumber ());
   ASSERT_EQ (42, result ().cast <int> ());
 }
