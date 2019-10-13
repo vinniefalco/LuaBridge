85c987ccb688d3b3f94f715ed319cc407d0dc351
Dmitry T <dmitry.a.tarakanov@gmail.com>
2019-10-13 21:18:01 +0300

    Update documentation

--- Tests/Source/IssueTests.cpp
+++ Tests/Source/IssueTests.cpp
@@ -153,15 +153,15 @@ enum class MyEnum
 template <typename T>
 struct EnumWrapper
 {
-    static typename std::enable_if<std::is_enum<T>::value, void>::type push(lua_State* L, T value)
-    {
-        lua_pushnumber (L, static_cast<std::size_t> (value));
-    }
+  static typename std::enable_if<std::is_enum<T>::value, void>::type push(lua_State* L, T value)
+  {
+    lua_pushnumber (L, static_cast<std::size_t> (value));
+  }
 
-    static typename std::enable_if<std::is_enum<T>::value, T>::type get(lua_State* L, int index)
-    {
-        return static_cast <T> (lua_tointeger (L, index));
-    }
+  static typename std::enable_if<std::is_enum<T>::value, T>::type get(lua_State* L, int index)
+  {
+    return static_cast <T> (lua_tointeger (L, index));
+  }
 };
 
 namespace luabridge {
